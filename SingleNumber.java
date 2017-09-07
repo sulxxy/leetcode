5ed78779a6fe129dc9889a5e2eda0de4a20118d2
Zhiwei Liu <cokeyliu@gmail.com>
2017-09-07 16:13:38 +0200

    updated

--- SingleNumber.java
+++ SingleNumber.java
@@ -1,30 +1,12 @@
-/*
- * Given an array of integers, every element appears twice except for one. Find that single one.
- *
- * Difficulty: easy
- */
 class Solution {
     public int singleNumber(int[] nums) {
         if(nums.length == 0){
             return 0;
         }
-        HashSet<Integer> hm = new HashSet<Integer>();
-        for(int i = 0; i < nums.length; i++){
-            if(hm.contains(nums[i])){
-                hm.remove(nums[i]);
-            }else{
-                hm.add(nums[i]);
-            }
+        int ret = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            ret ^= nums[i];
         }
-        return hm.iterator().next();
+        return ret;
     }
-    /* excellent solution from leetcode discussion forum*/
-    int singleNumber(int A[], int n) {
-    int result = 0;
-    for (int i = 0; i<n; i++)
-    {
-      result ^=A[i];
-    }
-    return result;
-  }
 }

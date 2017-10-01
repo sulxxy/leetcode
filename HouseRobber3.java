06119972fb0e79f19e42aaecb7785b1ad8115a35
Zhiwei Liu <cokeyliu@gmail.com>
2017-10-01 16:44:08 +0200

    updated

---
+++ HouseRobber3.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int rob(TreeNode root) {
+        return robHelper(root, new HashMap<TreeNode, Integer>());
+    }
+    
+    public int robHelper(TreeNode root, HashMap<TreeNode, Integer> map){
+        if(root == null){
+            return 0;
+        }
+        if(map.containsKey(root)){
+            return map.get(root);
+        }
+        
+        int val = 0;
+        
+        if(root.left != null){
+            val += robHelper(root.left.left, map) + robHelper(root.left.right, map);
+        }
+        
+        if(root.right != null){
+            val += robHelper(root.right.left, map) + robHelper(root.right.right, map);
+        }
+        
+        int ret = Math.max(val+root.val, robHelper(root.left, map) + robHelper(root.right, map) );
+        map.put(root, ret);
+        
+        return ret;
+    }
+}

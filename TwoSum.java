0102e5f1dafc8ff072dda2ad449aa8ee1891b273
Zhiwei Liu <cokeyliu@gmail.com>
2017-10-27 18:55:23 +0200

    formated

--- TwoSum.java
+++ TwoSum.java
@@ -13,10 +13,10 @@
 import java.util.*;
 
 public class TwoSum{
-	public static int[] twosum(int[] nums, int target){
-		int[] ret = new int[2];
-		HashMap<Integer, Integer> hashmap = new HashMap<Integer, Integer> ();
-		for(int i = 0; i < nums.length; i++){
+  public static int[] twosum(int[] nums, int target){
+    int[] ret = new int[2];
+    HashMap<Integer, Integer> hashmap = new HashMap<Integer, Integer> ();
+    for(int i = 0; i < nums.length; i++){
       if(hashmap.containsKey(nums[i])){
         if(nums[i] * 2 == target){
           ret[0] = hashmap.get(nums[i]);
@@ -24,34 +24,34 @@ public class TwoSum{
         }
       }
       else{
-			  hashmap.put(nums[i], i);
+        hashmap.put(nums[i], i);
       }
-		}
-		for(int j = 0; j < nums.length; j++){
-			if(hashmap.containsKey(nums[j])){
-				if(hashmap.containsKey(target-nums[j])){
-					int a0 = hashmap.get(nums[j]);
-					int a1 = hashmap.get(target - nums[j]);
-					if(a0 == a1){
-						continue;
-					}
-					else{
-						ret[0] = a0;
-						ret[1] = a1;
-						return ret;
-					}
-				}
-			}
-		}
-		return ret;
-	}
+    }
+    for(int j = 0; j < nums.length; j++){
+      if(hashmap.containsKey(nums[j])){
+        if(hashmap.containsKey(target-nums[j])){
+          int a0 = hashmap.get(nums[j]);
+          int a1 = hashmap.get(target - nums[j]);
+          if(a0 == a1){
+            continue;
+          }
+          else{
+            ret[0] = a0;
+            ret[1] = a1;
+            return ret;
+          }
+        }
+      }
+    }
+    return ret;
+  }
 
-	public static void main(String[] args){
-		int[] s = {3, 2, 4};
+  public static void main(String[] args){
+    int[] s = {3, 2, 4};
     int[] res = twosum(s, 6);
-		System.out.println(res[0]);
-		System.out.println(res[1]);
-		return ;
-	}
+    System.out.println(res[0]);
+    System.out.println(res[1]);
+    return ;
+  }
 }
 
